aa10f1706cedba1282f0c36f4b663133b556bb23
Mateus Bastazini Nascimento Talhacolo <112733464+MBastazini@users.noreply.github.com>
2024-04-30 15:11:42 -0300

    Adicionado background

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Background } from './components/user/Background'
+import './App.scss'
+
+function App() {
+  return (
+    <Background />
+  )
+}
+
+export default App
---
+++ client/src/components/user/Background.tsx
@@ -0,0 +1,19 @@
+const Background = () => {
+    return (
+        <div id='user-background'>
+            <div id="q1"></div>
+            <div id="q2"></div>
+            <div id="q3"></div>
+            <div id="q4"></div>
+            <div id="q5"></div>
+            <div id="q6"></div>
+            <div id="q7"></div>
+            <div id="q8"></div>
+            <div id="q9"></div>
+            <div id="q10"></div>
+            <div id="color"></div>
+        </div>
+    );
+}
+
+export { Background };
