28353ec2b8691fc3eec038fcf948662b1d6243e3
Gabriel Carraro Salzedas <gabriel.c.salzedas@unesp.br>
2024-05-03 12:05:05 -0300

    ConnectionDAO?

---
+++ server/src/DAO/ConnectionDAO.ts
@@ -0,0 +1,21 @@
+import { connect } from 'ts-postgres';
+
+export class ConnectionDAO {
+    private client: any;
+
+    constructor() {
+        this.client = connect({
+            host: 'localhost',
+            port: 5432,
+            database: 'aula',
+            user: 'postgres',
+            password: '123'
+        });
+    }
+
+    getConnection(): any {
+        return this.client;
+    }
+
+
+}
--- server/src/DAO/UserDAO.ts
+++ server/src/DAO/UserDAO.ts
@@ -1,29 +1,30 @@
-import { Client, ResultRow } from 'ts-postgres';
+import { ResultRow } from 'ts-postgres';
+import { ConnectionDAO } from './ConnectionDAO';
 
-const registerUser = async (client: Client, user: UserDTO) => {
-    const result = await client.query(
+const registerUser = async (connectionDAO: ConnectionDAO, user: UserDTO) => {
+    const result = await connectionDAO.getConnection().query(
         `INSERT INTO user (name, email, password) VALUES ($1, $2, $3) RETURNING id`,
         [user.name, user.email, user.password]
     );
     return result.rows[0].get('id');
 }
 
-const updateUser = async (client: Client, user: UserDTO) => {
-    await client.query(
+const updateUser = async (connectionDAO: ConnectionDAO, user: UserDTO) => {
+    await connectionDAO.getConnection().query(
         `UPDATE user SET name = $1, email = $2, password = $3 WHERE id = $4`,
         [user.name, user.email, user.password, user.id]
     );
 }
 
-const deleteUser = async (client: Client, user: UserDTO) => {
-    await client.query(
+const deleteUser = async (connectionDAO: ConnectionDAO, user: UserDTO) => {
+    await connectionDAO.getConnection().query(
         `DELETE FROM user WHERE id = $1`,
         [user.id]
     );
 }
 
-const listUsers = async (client: Client) => {
-    const result = await client.query('SELECT * FROM user');
+const listUsers = async (connectionDAO: ConnectionDAO) => {
+    const result = await connectionDAO.getConnection().query('SELECT * FROM user');
     const users: UserDTO[] = result.rows.map((row: ResultRow<any>) => {
         return {
             id: row.get('id'),
@@ -37,8 +38,8 @@ const listUsers = async (client: Client) => {
     return users;
 }
 
-const searchUserById = async (client: Client, user: UserDTO) => {
-    const result = await client.query('SELECT * FROM user WHERE id = $1', [user.id]);
+const searchUserById = async (connectionDAO: ConnectionDAO, id: number) => {
+    const result = await connectionDAO.getConnection().query('SELECT * FROM user WHERE id = $1', [id]);
     const row = result.rows[0];
     const userDTO: UserDTO = {
         id: row.get('id'),
