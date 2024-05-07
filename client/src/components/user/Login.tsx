ab2503e4302f186204c8ca5f568daa412c8aef52
Mateus Bastazini Nascimento Talhacolo <112733464+MBastazini@users.noreply.github.com>
2024-05-07 17:08:27 -0300

    Adicionado controller no client e divisão login / signup
    Metodos envolvendo fetch, banco de dados e validação de email, senha agora no controller e feia a divisão de Options, Logo e Login, Signup como componentes separados para aumentar a modularização, sem ser tudo no mesmo componente acho que é mais facil de manter.

---
+++ client/src/components/user/Login.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import Input from '../Input';
+import Options from "./Options";
+import Logo from './Logo';
+
+import { handleLogin } from '../../controllers/userController';
+import { showAlert } from '../../App';
+
+
+interface Props{
+    changeLoginPage: () => void
+
+}
+
+const Login = ({changeLoginPage}:Props) => {
+
+    //Fetch options
+    const [loading, setLoading] = useState(false);
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [remember, setRemember] = useState(false);
+
+    //A ideia e que, se for true, todos os inputs recebem as "caixinhas vermelhas" em volta,
+    //E não só de e-mail e senha, pedindo para que o usuario preencha os outros valores
+    const[isInputsValid, setIsInputsValid] = useState(false);
+
+    function handleEmailChange(e: React.ChangeEvent<HTMLInputElement>){
+        setEmail(e.target.value);      
+    }
+
+    function handlePasswordChange(e: React.ChangeEvent<HTMLInputElement>){
+        setPassword(e.target.value);     
+    }
+
+
+    async function handleLoginButtonClick(){
+        if (email.length == 0 || password.length == 0) {
+            setIsInputsValid(true);
+            showAlert('Preencha todos os campos');
+            return;
+        }
+        else{
+            setLoading(true);
+            await handleLogin(email, password, remember);
+            setLoading(false);
+        }
+    }
+
+
+    return (
+        <div id="inputArea">
+                <Logo type={'login'}/>
+
+                <div id="inputs">               
+                    <Input name="email" label="Email" onChange={handleEmailChange}
+                    valid={isInputsValid ? email.length > 0 : undefined}/> 
+                    <Input name="password" label="Senha" onChange={handlePasswordChange}
+                    type='password'
+                    valid={isInputsValid ? password.length > 0 : undefined}/>
+                </div>
+
+                <Options type={'login'} 
+                onClick={handleLoginButtonClick}
+                changeScreen={changeLoginPage}
+                loading={loading}
+                onRemember={setRemember}
+                />         
+            </div>
+        
+    )
+}
+
+/*<div className="center full-screen-size">
+            <Background signin={!isLogin}/>
+            
+        </div>
+*/
+
+/*
+
+{/*Botões do login /}
+
+*/
+export default Login;
---
+++ client/src/controllers/userController.ts
@@ -0,0 +1,122 @@
+import { showAlert } from "../App";
+
+const validadeEmail = (email: string): string[] => { //Deveria mudar string[] para uma interface??
+    let newIsEmailValid = ['@', '.'];
+
+    //Deve ter só 1 '@'
+    if (email.split('@').length - 1 == 1) {
+        newIsEmailValid = newIsEmailValid.filter((char) => char !== '@');
+    } 
+
+    if (email.includes('.')){
+        newIsEmailValid = newIsEmailValid.filter((char) => char !== '.')
+    } 
+
+    return newIsEmailValid;
+}
+
+const validadePassword = (password: string): number[] => {
+
+    /*Password regras:
+    1. Pelo menos 8 caracteres
+    2. Pelo menos uma letra maiúscula
+    3. Pelo menos um número
+    4. Pelo menos um caractere especial //não existe mais
+    */
+
+    let newIsPasswordValid = [1, 2, 3];
+    if(password.length >= 8){
+        newIsPasswordValid = newIsPasswordValid.filter((rule) => rule !== 1)
+    }
+
+    if(password.match(/[A-Z]/)) {
+        newIsPasswordValid = newIsPasswordValid.filter((rule) => rule !== 2)
+    }
+
+    if(password.match(/[0-9]/)) {
+        newIsPasswordValid = newIsPasswordValid.filter((rule) => rule !== 3)
+
+    }
+
+    /*e.target.value.match(/[!@#$%^&*_]/) ?
+    newIsPasswordValid = newIsPasswordValid.filter((rule) => rule !== 4)
+    : !newIsPasswordValid.includes(4) && (newIsPasswordValid = [...newIsPasswordValid, 4]);*/
+
+    return newIsPasswordValid;
+}
+
+async function handleSignup(name: string, email: string, password: string, remember: boolean): Promise<boolean> {
+
+    //await new Promise(resolve => setTimeout(resolve, 3000));
+    //return true;
+
+    try {
+        const data = {
+            name: name,
+            email: email,
+            password: password,
+            remember: remember
+        };
+
+        const response = await fetch('/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+            if (responseData.message === 'Perfil já existe') {
+                throw new Error('Perfil já existe');
+            }
+            throw new Error(response.statusText);
+        } else {
+            return true;
+        }
+    } catch (err: any) {
+        console.log(err);
+        showAlert("Erro: " + err.message);
+        return false;
+    }
+}
+
+async function handleLogin(email: string, password: string, remember: boolean): Promise<boolean> {
+        //await new Promise(resolve => setTimeout(resolve, 3000));
+        //return true;
+    try {
+        const data = {
+            email: email,
+            password: password,
+            remember: remember
+        };
+
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+            if (responseData.message === 'Perfil não encontrado') {
+                throw new Error('E-mail ou senha incorretos');
+            }
+            throw new Error(response.statusText);
+        } else {
+            //console.log('L');
+            //window.location.href = "https://www.youtube.com/watch?v=KZzJlyjMJws";
+            return true;
+        }
+    } catch (err: any) {
+        console.log(err);
+        showAlert("Erro: " + err.message);
+        return false;
+    }
+}
+
+
+export {validadeEmail, validadePassword, handleLogin, handleSignup}
