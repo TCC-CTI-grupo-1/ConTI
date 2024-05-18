22d089e59567841be851d964f515f65dd06e3301
MAC417 <maclocal@MacBook-Air-de-MAC417.local>
2024-05-18 09:33:25 -0300

    Mudanças

--- client/src/components/user/profile/Profile.tsx
+++ client/src/components/user/profile/Profile.tsx
@@ -46,6 +46,8 @@ const Profile = () => {
                     <div className="options" ref={options}>
                         <a className="active">Status do usuario</a>
                         <a>Configurações da conta</a>
+                        <a>Area do administrador</a>
+                        <div className='selected-line'></div>
                     </div>
                 </div>
                 <div className="inversed-border"></div>
