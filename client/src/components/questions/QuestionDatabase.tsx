9be5edf5749a5f13f465ec86a3243bd4f1e0cde4
Mateus Bastazini Nascimento Talhacolo <112733464+MBastazini@users.noreply.github.com>
2024-06-03 19:13:16 -0300

    Melhorada tela de questão individual!

--- client/src/components/questions/QuestionDatabase.tsx
+++ client/src/components/questions/QuestionDatabase.tsx
@@ -2,25 +2,51 @@
 import Background from '../user/Background'
 import Navbar from '../Navbar'
 import Filters from './Filters'
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import QuestionDetail from './QuestionDetail';
+import { useNavigate } from 'react-router-dom';
 
 const QuestionDatabase = () => {
-    
-  return (
+    const { id } = useParams(); // Acessando o ID da pergunta da rota
+    const navigate = useNavigate();
+    const [hasQuestion, setHasQuestion] = useState<boolean>(false);
+    useEffect(() => {
+        if (id) {
+            setHasQuestion(true);
+            //alert('Question ID: ' + id);
+        } else {
+            setHasQuestion(false);
+            //alert('No question ID');
+        }
+    }, [id]);
+
+    return (
     <div id="database" className="flex-container full-screen-size">
             <Background variant='white'/>
             <Navbar screen="database"/>
             <div className="container">
                 <div className="header">
-                    <h1>Banco de questões</h1>
-                    <p>Todas as questões do nosso sistema em um só lugar, aplique os filtros desejados e aproveite as questões</p>
+                    
+                    {hasQuestion ? <h1><a
+                    onClick={() => {
+                        navigate('/questions');
+                    }}>Banco de questões</a> &gt; <a>{id}</a></h1> :<h1>Banco de questões</h1>}
+
+                    {hasQuestion 
+                    ? <a onClick={() => {
+                        navigate('/questions');
+                    }}>Voltar a tela de filtros</a>
+                    : <p>Todas as questões do nosso sistema em um só lugar, aplique os filtros desejados e aproveite as questões</p>}
+                
                 </div>
                 <div className="inversed-border"></div>
                 <div className="content">
-                    <Filters />
+                    {hasQuestion ? <QuestionDetail questionID={id as string}/> : <Filters />}
                 </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default QuestionDatabase
--- client/src/components/questions/QuestionDetail.tsx
+++ client/src/components/questions/QuestionDetail.tsx
@@ -1,68 +1,66 @@
 // QuestionDetail.js
-import { useParams } from 'react-router-dom';
-import Navbar from '../Navbar';
+
 import { Button } from '@chakra-ui/react';
 
-function QuestionDetail() {
-  const { id } = useParams(); // Acessando o ID da pergunta da rota
-  // Aqui você pode usar o ID para carregar os detalhes da pergunta
-  return (
-    <div id="database" className="flex-container full-screen-size">
-            <Navbar screen="database"/>
-            <div className="container">
-                <div className="header">
-                    <h1><a>Banco de questões</a> &gt; <a>{id}</a></h1>
-                </div>
-                <div className="inversed-border"></div>
-                <div className="content">
+interface Props{
+    questionID: string;
+}
 
-                    <div className='box question'>
-                        <p>CTI &gt; 2023 &gt; Ciências Humanas &gt; Fontes Energéticas </p>
-                        <h3>O aumento da demanda global por recursos energéticos tem gerado preocupações quanto à segurança energética 
-                            e um maior interesse em buscar fontes de energia  consideradas  sustentáveis e renováveis.  Diante desse cenário, 
-                            são exemplos de fontes sustentáveis e renováveis as que constam em: 
-                        </h3>
-                        <div className="alternatives">
-                            <div className='active'>
-                                <span>
-                                    <h3>A</h3>
-                                </span>
-                                <p>Também acho</p>
-                            </div>
-                            <div>
-                                <span>
-                                    <h3>A</h3>
-                                </span>
-                                <p>Também acho</p>
-                            </div>
-                            <div>
-                                <span>
-                                    <h3>A</h3>
-                                </span>
-                                <p>Também acho</p>
-                            </div>
-                            <div>
-                                <span>
-                                    <h3>A</h3>
-                                </span>
-                                <p>Também acho</p>
-                            </div>
-                            <div>
-                                <span>
-                                    <h3>A</h3>
-                                </span>
-                                <p>Também acho</p>
-                            </div>
-                        </div>
-                        <div className="options">
-                            <Button colorScheme="blue" size="lg">Responder</Button>
-                            <Button colorScheme="blue" size="lg" variant='outline'>Ver resolução comentada</Button>
-                        </div>
-                    </div>
+function QuestionDetail({questionID}: Props) {
+
+  return (
+    <>
+        <div className='box question'>
+            <p>CTI &gt; 2023 &gt; Ciências Humanas &gt; Fontes Energéticas </p>
+            <h3>O aumento da demanda global por recursos energéticos tem gerado preocupações quanto à segurança energética 
+                e um maior interesse em buscar fontes de energia  consideradas  sustentáveis e renováveis.  Diante desse cenário, 
+                são exemplos de fontes sustentáveis e renováveis as que constam em: 
+            </h3>
+            <div className="alternatives">
+                <div className='active'>
+                    <span>
+                        <h3>A</h3>
+                    </span>
+                    <p>Também acho</p>
+                </div>
+                <div>
+                    <span>
+                        <h3>A</h3>
+                    </span>
+                    <p>Também acho</p>
+                </div>
+                <div>
+                    <span>
+                        <h3>A</h3>
+                    </span>
+                    <p>Também acho</p>
+                </div>
+                <div>
+                    <span>
+                        <h3>A</h3>
+                    </span>
+                    <p>Também acho</p>
+                </div>
+                <div>
+                    <span>
+                        <h3>A</h3>
+                    </span>
+                    <p>Também acho</p>
                 </div>
             </div>
+            <div className="options">
+                <Button colorScheme="blue" size="lg">Responder</Button>
+                <Button colorScheme="blue" size="lg" variant='outline'>Ver resolução comentada</Button>
+            </div>
         </div>
-
+        <div className="more-options">
+            <p>Explorar questões similares</p>
+            <div>
+                <Button colorScheme="blue" size="lg" variant='outline'>Anterior</Button>
+                <Button colorScheme="blue" size="lg" variant='outline'>Próxima</Button>
+            </div>
+        </div>
+    </>
   );
 }
 
