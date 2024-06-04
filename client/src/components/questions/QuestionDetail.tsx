4af5af0481d2a99c9451994fdb917c69c7a8fd23
Mateus Bastazini Nascimento Talhacolo <112733464+MBastazini@users.noreply.github.com>
2024-06-04 09:30:18 -0300

    Botão de responder funciona e pode-se mudar a alternativa

--- client/src/components/questions/QuestionDetail.tsx
+++ client/src/components/questions/QuestionDetail.tsx
@@ -1,14 +1,62 @@
-// QuestionDetail.js
-
 import { Button } from '@chakra-ui/react';
+import { useRef, useEffect, useState, useCallback } from 'react';
 
-interface Props{
+interface Props {
     questionID: string;
 }
 
-function QuestionDetail({questionID}: Props) {
+function QuestionDetail({ questionID }: Props) {
 
-  return (
+    const questionRef = useRef<HTMLDivElement>(null);
+
+    const [showAnswer, setShowAnswer] = useState(false);
+
+    const correctAnswer = 'C';
+
+    const handleClick = useCallback((event: Event) => {
+        const target = event.currentTarget as HTMLElement;
+        const alternatives = questionRef.current?.querySelectorAll('.alternatives div');
+
+        if (target.classList.contains('active')) {
+            target.classList.remove('active');
+        } else {
+            alternatives?.forEach((alternative) => {
+                alternative.classList.remove('active');
+            });
+            target.classList.add('active');
+        }
+
+    }, []);
+
+    useEffect(() => {
+        const alternatives = questionRef.current?.querySelectorAll('.alternatives div');
+        if (!showAnswer) {
+            console.log('add click event listener');
+            alternatives?.forEach((alternative) => {
+                alternative.addEventListener('click', handleClick);
+                
+                if (alternative.querySelector('h3')?.textContent === correctAnswer) {
+                    alternative.classList.add('correct');
+                }
+            });
+        } else {
+            // remove click event listener
+            console.log('remove click event listener');
+            alternatives?.forEach((alternative) => {
+                alternative.removeEventListener('click', handleClick);
+            });
+        }
+
+        // Cleanup function to remove event listeners when the component unmounts or when showAnswer changes
+        return () => {
+            console.log('cleanup event listeners');
+            alternatives?.forEach((alternative) => {
+                alternative.removeEventListener('click', handleClick);
+            });
+        };
+    }, [showAnswer, handleClick]);
+
+    return (
     <>
         <div className='box question'>
             <p>CTI &gt; 2023 &gt; Ciências Humanas &gt; Fontes Energéticas </p>
@@ -16,13 +64,7 @@ function QuestionDetail({questionID}: Props) {
                 e um maior interesse em buscar fontes de energia  consideradas  sustentáveis e renováveis.  Diante desse cenário, 
                 são exemplos de fontes sustentáveis e renováveis as que constam em: 
             </h3>
-            <div className="alternatives">
-                <div className='active'>
-                    <span>
-                        <h3>A</h3>
-                    </span>
-                    <p>Também acho</p>
-                </div>
+            <div className={"alternatives " + (showAnswer ? 'showCorrect' : '')} ref={questionRef}>
                 <div>
                     <span>
                         <h3>A</h3>
@@ -31,25 +73,34 @@ function QuestionDetail({questionID}: Props) {
                 </div>
                 <div>
                     <span>
-                        <h3>A</h3>
+                        <h3>B</h3>
                     </span>
                     <p>Também acho</p>
                 </div>
                 <div>
                     <span>
-                        <h3>A</h3>
+                        <h3>C</h3>
                     </span>
                     <p>Também acho</p>
                 </div>
                 <div>
                     <span>
-                        <h3>A</h3>
+                        <h3>D</h3>
+                    </span>
+                    <p>Também acho</p>
+                </div>
+                <div>
+                    <span>
+                        <h3>E</h3>
                     </span>
                     <p>Também acho</p>
                 </div>
             </div>
             <div className="options">
-                <Button colorScheme="blue" size="lg">Responder</Button>
+                <Button colorScheme="blue" size="lg"
+                onClick={() => {
+                    setShowAnswer(!showAnswer);
+                }}>{showAnswer ? 'Ocultar' : 'Responder'}</Button>
                 <Button colorScheme="blue" size="lg" variant='outline'>Ver resolução comentada</Button>
             </div>
         </div>
@@ -61,7 +112,7 @@ function QuestionDetail({questionID}: Props) {
             </div>
         </div>
     </>
-  );
+    );
 }
 
 export default QuestionDetail;
