88a537c4ced7c0b82e02565414860fb60f07d889
Gabriel Carraro Salzedas <gabriel.c.salzedas@unesp.br>
2024-06-08 09:29:10 -0300

    Consertado erro referente à obtenção das informações da sessão

--- server/src/http/controllers/getProfileSessionController.ts
+++ server/src/http/controllers/getProfileSessionController.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 
 export async function getProfileSessionController (req: Request, res: Response) {
     if (!req.session.isLoggedIn) {
-        res.status(400).json({ message: 'Usuário não logado' });
+        return res.status(400).json({ message: 'Usuário não logado' });
     }
+    console.log(req.session.profile)
     res.json({ profile: req.session.profile });
 }
