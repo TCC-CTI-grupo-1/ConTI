884e04ea821d4e5a74cce7285361cabea7dde457
Fernando Ellerbrock Theodoro <135258926+Sekqies@users.noreply.github.com>
2024-08-18 14:50:20 -0300

    Iniciado desenvolvimento de test_builder
    Implementado algoritmo para a construção de provas aleatórias (falta conexão ao banco e integração ao resto do aplicativo),
    Implementado algoritmo para a construção de provas com dificuldade dependendo da dificuldade individual de cada uma das questões

--- test_builder.ts
+++ test_builder.ts
@@ -1,21 +1,47 @@
 import {MockTestDTO} from "./server/src/DTO/MockTestDTO";
 1
-enum TestDifficulty{
+
+
+
+
+enum DifficultyLevel{
     EASY,
     MEDIUM,
-    HARD
+    HARD,
+    MIMIC,
+    RANDOM
 };
 
 enum DifficultyType{
-    SUBJECT,INDIVIDUAL
+    AREA,INDIVIDUAL,SPECIAL
+}
+const buildProportions = function(...args:number[])
+{
+    return {
+        easy: args[0],
+        medium: args[1],
+        hard: args[2]
+    }
+}
+
+const difficultyProportions =  {
+    [DifficultyLevel.EASY] : buildProportions(3,2,1),
+    [DifficultyLevel.MEDIUM] : buildProportions(1,1,1),
+    [DifficultyLevel.HARD] : buildProportions(1,2,3)
+}
+
+const difficultyToErrorRatioCondition = {
+    "easy" : "total_correct_answers / total_answers > 0.75",
+    "medium" : "total_correct_answers / total_answers > 0.5 AND total_correct_answers / total_answers <=0.5",
+    "hard" : "total_correct_answers / total_answers < 0.5"
 }
 
 
 
 interface ITestBlueprint{
     totalQuestions:number,
-    questionBySubject:object,
-    difficultyRating:TestDifficulty,
+    questionBySubject:{[key:string]:number},
+    difficultyLevel:DifficultyLevel,
     difficultyType:DifficultyType
 }
 
@@ -23,13 +49,21 @@ interface ITestBuilder{
     testBlueprint:TestBlueprint[] | undefined
 }
 
+const nameToId = {
+    "portugues" : 2
+}
+
+function getRandomInInterval(min,max)
+{
+    return Math.floor(Math.random() * (max-min) +min);
+} 
 
 class TestBlueprint implements ITestBlueprint{
     totalQuestions: number;
     questionBySubject: {[key:string]:number};
-    difficultyRating: TestDifficulty;
+    difficultyLevel: DifficultyLevel;
     difficultyType: DifficultyType;
-    constructor(totalQuestions=50,questionBySubject={"lingua_portuguesa":15,"matematica":15,"ciencias_biologicas":15,"ciencias_humanas":5},difficultyRating=TestDifficulty.MEDIUM,difficultyType=DifficultyType.INDIVIDUAL)
+    constructor(totalQuestions=50,questionBySubject={"lingua_portuguesa":15,"matematica":15,"ciencias_biologicas":15,"ciencias_humanas":5},difficultyRating=DifficultyLevel.MEDIUM,difficultyType=DifficultyType.INDIVIDUAL)
     {
         this.totalQuestions = totalQuestions;
         this.questionBySubject = questionBySubject;
@@ -40,22 +74,91 @@ class TestBlueprint implements ITestBlueprint{
                 totalSum+=value;
             }
         }
-        console.warn("Erro na construção da prova: Número de questoes por materia deve ser igual ao numero total");
-        this.difficultyRating = difficultyRating;
+        console.error("Erro na construção da planta da prova: Número de questões por matéria deve ser igual ao número total");
+        this.difficultyLevel = difficultyRating;
         this.difficultyType = difficultyType;
     }
 }
 
 class TestBuilder{
-    testBlueprint:TestBlueprint[] | undefined
+    testBlueprintList:TestBlueprint[] | undefined
     constructor(testBlueprint:TestBlueprint[] | undefined)
     {
-        this.testBlueprint = testBlueprint;
+        this.testBlueprintList = testBlueprint;
     }
-
+    buildAll()
+    {
+        if(this.testBlueprintList) {
+            for(const test of this.testBlueprintList){
+            }
+        }
+        else {
+            console.error("Erro: TestBlueprintList está vazio");
+        }
+        
+    }
     buildTest(blueprint:TestBlueprint)
     {
-
+        let questionList: { [key: string]: any[] } = {};
+        for (const subject in blueprint.questionBySubject) {
+            const numberOfQuestions = blueprint.questionBySubject[subject];
+            if(blueprint.difficultyLevel === DifficultyLevel.RANDOM)
+            {
+                const row = this.runQuery(`SELECT * FROM questions WHERE subject_id = ${nameToId[subject]} ORDER BY RANDOM()`)
+                questionList[subject] = []
+                for(let i=0;i<numberOfQuestions;i++)
+                {
+                    questionList[subject].push(row[i]);
+                }
+            }
+            if(blueprint.difficultyType === DifficultyType.INDIVIDUAL)
+            {
+                //Ou seja, a dificuldade é construída pela dificuldade individual das questões. Uma questão difícil sempre vai ser difícil
+                const proportions = difficultyProportions[blueprint.difficultyLevel];
+                let maxval = 0;
+                for (const val of proportions) maxval+=val;
+                const difficultyCountInSubject =  {}
+                for (const subject in blueprint.questionBySubject)
+                {
+                    const questionCount = blueprint.questionBySubject[subject]
+                    difficultyCountInSubject[subject] = {}
+                    questionList[subject] = []
+                    for (const proportion in proportions)
+                    {
+                        if(proportion == "medium")
+                        {
+                            difficultyCountInSubject[subject] = {proportion: Math.ceil(questionCount - Math.floor(questionCount * proportions['easy'] /maxval) - Math.floor(questionCount * proportions['hard'] / maxval))}
+                        }
+                        else
+                            difficultyCountInSubject[subject] = {proportion: Math.floor(proportions[proportion] * questionCount / maxval)}
+                    }
+                    for (const difficulty in difficultyCountInSubject[subject])
+                    {
+                        const row = this.runQuery(`SELECT * FROM question WHERE ${difficultyToErrorRatioCondition[difficulty]} ORDER BY RANDOM()`)
+                        for(let i=0;i<difficultyCountInSubject[subject][difficulty];i++)
+                        {
+                            questionList[subject].push(row[i]);
+                        }
+                    }
+                   
+                }
+            }
+            if(blueprint.difficultyType === DifficultyType.AREA)
+            {
+                alert("Não implementei ainda!");
+                //TODO: pensar no algoritmo que constrói baseado nos indicadores do usuário
+            }
+            if(blueprint.difficultyLevel === DifficultyLevel.MIMIC)
+            {
+                alert("Não implementado")
+                //A ideia disso é imitar
+            }
+        }
+        return questionList;
+    }
+    runQuery(query:string)
+    {
+        return ["porra nenhuma"]
     }
 }
 
