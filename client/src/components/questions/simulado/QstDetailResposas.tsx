d249bfbdfe5e0cc6d15679eaea269245a0e96a3f
Mateus Bastazini Nascimento Talhacolo <112733464+MBastazini@users.noreply.github.com>
2024-09-09 10:35:07 -0300

    Respostas.tsx funcionando
    - Recebe as respostas corretamente e as apresenta
    
    - Atenção, em Filtros.tsx os erros de "Ocorreu um erro ao carregar a alternativa [0] e [1] estão sem o showAlert, dado que eles apareciam

---
+++ client/src/components/questions/simulado/QstDetailResposas.tsx
@@ -0,0 +1,51 @@
+import { useState, useRef, useEffect, useCallback } from 'react';
+import { questionInterface, respostaInterface } from '../../../controllers/interfaces';
+import LocalButton from '../../Button';
+import { showAlert } from '../../../App';
+
+interface Props {
+    question: questionInterface;
+    answers: respostaInterface[];
+    selectedAnswer: number;
+}
+
+const QstDetailRespostas = ({question, answers, selectedAnswer}: Props) => {
+
+    const questionRef = useRef<HTMLDivElement>(null);
+    const alternativasRef = useRef(new Array());
+
+    return (
+        <>
+            {question === undefined ? <h1>Erro ao carregar questão</h1> : 
+            <div className={'box question'}>
+                <p>CTI &gt; 2023 &gt; Ciências Humanas &gt; Fontes Energéticas </p>
+                <h4>
+                {question.question_text}
+                </h4>
+                <div className={"alternatives showCorrect"} ref={questionRef}>
+                    
+                    {answers.map((alternative, index) => (
+                        <div key={index} ref={(element) => alternativasRef.current.push(element)} className={(alternative.id === selectedAnswer ? 'active' : '')
+                            + ' ' + (alternative.is_correct ? 'correct' : '')
+                        }>
+                            <span>
+                                <p> {alternative.question_letter} </p>
+                            </span>
+                            <p> {alternative.answer} </p>
+                        </div>
+                    ))}
+    
+                </div>
+                <div className="options">
+                        <LocalButton colorScheme="blue" variant='outline'
+                        onClick={() => {showAlert("Em construção", "warning")}}>Ver resolução comentada</LocalButton>
+                    
+                </div>
+            </div>
+            }
+        </>
+        
+        );
+}
+
+export default QstDetailRespostas
