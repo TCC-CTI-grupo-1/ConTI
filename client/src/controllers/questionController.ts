a1a9938a19d0b9c8fc2a079722b2ac2ea42a2faa
Fernando Ellerbrock Theodoro <135258926+Sekqies@users.noreply.github.com>
2024-09-10 10:17:02 -0300

    Consertado erro de filtros
    Agora, se você possuir um filtro vazio, a tela filters.tsx vai retornar a lista de questões correta (Último commit não

--- client/src/controllers/questionController.ts
+++ client/src/controllers/questionController.ts
@@ -81,10 +81,7 @@ export async function handleGetFilteredQuestions(filters: questionFilters): Prom
         if (!response.ok) {
             throw new Error(responseData.message);
         } else {
-            let question: questionInterface = {"id":904,"question_text":"Em um estudo sobre solo, foi utilizada uma coluna de vidro, com uma torneira adaptada em sua extremidade, conforme ilustração. Em seguida, 100 mL de areia seca foram coloca- dos dentro da coluna. Então, lentamente, foi acrescentado água, até que o nível da água, após preencher os espaços entre os grãos, atingisse a marca de 100 mL. Após medir a quantidade de água colocada na coluna (1), a torneira foi aberta e foi medido o tempo em segundos (2), no qual toda a água presente na coluna demorou para escoar pela torneira. As propriedades do solo que foram medidas em 1 e 2 foram, respectivamente,","question_year":2020,"total_answers":0,"total_correct_answers":0,"difficulty":"facil","additional_info":"","area_id":3,"question_creator":"VUNESP","official_test_name":"CTI","question_number":31,"has_image":false,"has_latex":false};
-            return [
-                question
-            ];
+            return responseData.questions;
         }
 
     } catch (err: any) {
