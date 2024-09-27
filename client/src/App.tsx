5297a9187915632e3470de70a0c4486bd21a4fb6
Mateus Bastazini Nascimento Talhacolo <112733464+MBastazini@users.noreply.github.com>
2024-09-27 11:59:34 -0300

    Home sem rotas.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import AlertBox from './components/AlertBox';
-import User from './components/user/login/User';
-import Home from './components/home/Home';
-import Profile from './components/user/profile/Profile';
-import History from './components/history/History';
-import SimuladoVer from './components/questions/simulado/VerSimulado';
 import QuestionDatabase from './components/questions/QuestionDatabase';
-import AllTests from './components/questions/AllTests';
-import NewTest from './components/newtest/NewTest';
-import Admnistrador from './components/Administrador';
-import { handleGetUser } from './controllers/userController';
-import { useEffect, useState } from 'react';
-import AboutUs from './components/aboutUs/AboutUs';
-
-//Middlewares
-import Middleware from './middlewares/Middleware';
-
-
-import { Routes, Route } from "react-router-dom"
-import SimuladoFrame from './components/questions/simulado/FrameSimulado';
 
 const showAlert = (message: string, 
     type?: 'error' | 'success' | 'warning' | 'info',
@@ -37,54 +19,9 @@ const showAlert = (message: string,
 
 function App() {
 
-
-    const debugging = true;
-
-    if(!debugging) console.log = function(){}
-    
-
-    const middleware = new Middleware();
-
-    const [loading, setLoading] = useState(true);
-
-    async function checkUser(){
-        const user = await handleGetUser();
-        localStorage.setItem('isLoggedIn', 'false');
-        if(user)
-        {
-            localStorage.setItem('isLoggedIn', 'true');
-        }
-        setLoading(false);
-    }
-
-    useEffect(() => {
-        checkUser();
-    }, []);
-
     return (
         <div className='app'>
-          <div id='alert-box-container'></div>
-          { loading && false ? <div><h1>Carregando o literal fucking app</h1> {/* Re-inverter para true e descobrir pq o onload não funciona */}
-          <p>Demorando muito? tente novamente:</p>
-          <button onClick={checkUser}>TENTAR</button>
-          </div> :
-              <Routes>
-                  <Route path='/' element={ <Home /> } />
-                  <Route path='/newtest' element={middleware.routeToDisplay(['isLoggedIn'], <NewTest />)} />
-                  <Route path='/login' element={ <User /> } />
-
-                  <Route path='/aboutUs' element={<AboutUs />} />
-                  <Route path='/profile' element={middleware.routeToDisplay(['isLoggedIn'], <Profile />)} />
-                  <Route path='/history' element={middleware.routeToDisplay(['isLoggedIn'], <History />) } />
-
-                  <Route path='/alltests' element={ <AllTests /> } />   
-                  <Route path='/questions' element={<QuestionDatabase />} />
-                  <Route path='/questions/:id' element={<QuestionDatabase />} />
-                  <Route path='/test' element={<SimuladoFrame />} />
-                  <Route path='/simulado/:id' element={<SimuladoVer />} />
-                  <Route path='/adm' element={<Admnistrador />} />
-                </Routes>
-          }
+          <QuestionDatabase />
         </div>
     );
 }
