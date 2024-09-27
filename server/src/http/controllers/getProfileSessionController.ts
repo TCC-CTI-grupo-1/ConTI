027395711272862eb9400fd27fbcb8db7be77995
Gabriel Carraro Salzedas <gabriel.c.salzedas@unesp.br>
2024-09-27 12:11:00 -0300

    Alterações com o cookie (não existirá mais [acho])

--- server/src/http/controllers/getProfileSessionController.ts
+++ server/src/http/controllers/getProfileSessionController.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
 
 export async function getProfileSessionController (req: Request, res: Response) {
+    console.log("1")
     if (!req.session.isLoggedIn) {
-        return res.status(400).json({ message: 'Usuário não logado' });
+        if (!req.cookies.sid) {
+            console.log('Usuário não logado');
+            res.status(401).json({ message: 'Usuário não logado' });
+        }
+        console.log('Reconhecendo sessão');
+        req.session.id = req.cookies.sid;
+        req.session.isLoggedIn = true;
     }
+    console.log('Retornando perfil');
     res.json({ profile: req.session.profile });
 }
