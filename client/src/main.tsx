6a7f15bfde92a9738779c8cf5cd2b33b070a2e22
Mateus Bastazini Nascimento Talhacolo <112733464+MBastazini@users.noreply.github.com>
2024-09-27 10:45:20 -0300

    Novo css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
+import './home.scss'
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 
