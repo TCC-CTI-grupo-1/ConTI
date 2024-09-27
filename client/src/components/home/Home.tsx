f9f74bf9e6beeb2503eb4f45d7afc19fdde8d312
janainatoledo <janainatoledo04@gmail.com>
2024-09-27 10:13:34 -0300

    começo da home logada

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,21 +1,60 @@
-import Navbar from "../Navbar"
 
+import { Input } from "@chakra-ui/react";
+import Navbar from "../Navbar"
+import { useState } from 'react';
+import { Button, Spinner } from "@chakra-ui/react"
+
+//TEM QUE TER A DIVISÃO DO LOGADO E NÃO LOGADO
 const Home = () => {
   return (
     <div id="home" className="flex-container full-screen-size">
             <Navbar screen="home"/>
             <div className="container">
                 <div className="header">
-                    <h1>Home</h1>
+                    <h1>Olá, __</h1>
                 </div>
-                <div className="inversed-border"></div>
                 <div className="content">
-                    <h1>Home</h1>
-                    <h3>Atualmente em construção</h3>
+                    {//daily_c_graphic é a div que inclui questão diaria
+                    // e taxa de acertos na mesma linha
+                    }
+                    <div className="daily_c_graphic">
+                <div className="dly_question">
+                    <h2>Título da Caixa que deve ser a questão diaria</h2>
+                    <p> colocar aqui a logica e back da questao</p>
+                    <button className="b_testejana">Responder questão diaria</button>
+                </div>
+                 <div className="taxa_acerto">
+                    <p> Taxa de acerto nos simulados</p> 
+                    </div> 
+                    </div> 
+                    <div className="simuladobanco">
+                        <div className="simulado">
+                        <p>teste suas habilidades com um simulado rapido</p>
+                        <Button colorScheme="blue" size="lg" variant="solid" onClick={() =>
+                            {
+                            //Voltar para a tela anterior
+                                  alert('Em construção');
+                            }
+                            }>Iniciar simulado
+                        </Button>
+                        </div>
+                        <div className="acessar_questoes"> 
+                            <p> Veja nosso banco de questões completo</p>
+                            <Button name="btn_banco" colorScheme="gray" size="lg" variant="solid" onClick={() =>
+                            {
+                            //Voltar para a tela anterior
+                                  alert('Em construção');
+                            }
+                            }>Acessar banco
+                        </Button>
+                        </div>   
+                    </div>
+                 
                 </div>
             </div>
         </div>
   )
 }
+
 
 export default Home
