b6e22df22a570904ade24a041cce51bcfb778053
doroshtm <gabriel.c.salzedas@unesp.br>
2024-10-02 11:55:26 -0300

    server.ts finalizado

--- server/src/server.ts
+++ server/src/server.ts
@@ -66,4 +66,4 @@ app.use(express.static('uploads'));
 
 routes(app);
 
-app.listen(port, () => { console.log("Server is running on port 3001")} );
+app.listen(port, () => { console.log("Server is running on port " + port) });
