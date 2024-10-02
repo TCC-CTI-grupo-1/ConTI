8e20656fbf6638d78c91f68a5f9ff53025a1a85c
Fernando Ellerbrock Theodoro <135258926+Sekqies@users.noreply.github.com>
2024-10-02 17:31:10 -0300

    AAaAAAA

--- client/src/components/questions/simulado/FrameSimulado.tsx
+++ client/src/components/questions/simulado/FrameSimulado.tsx
@@ -1,26 +1,32 @@
 import Simulado from "./Simulado"
 import { useState, useEffect } from "react"
 import { questionInterface, respostaInterface, simuladoInterface } from "../../../controllers/interfaces"
-import { handlePostSimulado, generateNewSimulado, handlePutSimulado} from "../../../controllers/mockTestController"
-import {handleGetAnswersByQuestionsIds, handleIncrementAnswers } from "../../../controllers/answerController"
+import { handleGetQuestion, handlePostSimulado, generateNewSimulado, handleGetAnswersByQuestionsIds } from "../../../controllers/userController"
+import date from 'date-and-time'
 import { useNavigate } from "react-router-dom"
 
-// import {
-//     Spinner
-//   } from '@chakra-ui/react'
-import { showAlert } from "../../../App"
-import { handleIncrementProfileAnswers, handleIncrementProfileMockTest } from "../../../controllers/userController"
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    useDisclosure,
+    Button,
+    Spinner
+  } from '@chakra-ui/react'
 
-type questionMapInterface = {
-    question: questionInterface;
-    answers: respostaInterface[];
-}[];
-type questionMapResultInterface = [number, (number | null)][];  
+type questionMapInterface = questionInterface[];
+type questionMapResultInterface = [number, (string | null)][];  
 
 const SimuladoFrame = () => {
+    const {onOpen, onClose, isOpen} = useDisclosure();
+
     const [questionsHashMap, setQuestionsHashMap] = useState<questionMapInterface | null>(null);
     const [loading, setLoading] = useState(true);
     const [simulado, setSimulado] = useState<simuladoInterface>({} as simuladoInterface);
+    const [isSimuladoFinished, setIsSimuladoFinished] = useState<boolean>(false);
 
     //A unica utilidade disso é impedir que o usuario clique no botão de finalizar simulado mais de uma vez enquanto ele está sendo corrigido
     const [isSimuladoAwaitActive, setIsSimuladoAwaitActive] = useState(false);
@@ -42,145 +48,124 @@ const SimuladoFrame = () => {
         handleFinishSimulado(respostas);
     }
 
-    
-
     async function handleFinishSimulado(respostas: questionMapResultInterface){
+        onOpen();
         setIsSimuladoAwaitActive(true);
-        let totalCorrectAnswers = 0;
-        let totalAnswers = 0;
-        const date = new Date();
-        const dateSimulado = new Date(simulado.creation_date_tz);
-        const timeSpentInMinutes = Math.floor((date.getTime() - dateSimulado.getTime()) / 60000);
-        
-        if(questionsHashMap === null) return;
-        
-        respostas.forEach((value, index) => {
-            ++totalAnswers;
-            const question = questionsHashMap[index];
-            if (question !== undefined && value[1] !== null) {
-                const correctAnswer = question.answers.find((answer) => answer.is_correct === true);
-                if (correctAnswer !== undefined && correctAnswer.id === value[1]) {
-                    ++totalCorrectAnswers;
-                }
-            }
-        });
-        
-        const novoSimulado = {
-            ...simulado,
-            total_correct_answers: totalCorrectAnswers,
-            total_answers: totalAnswers,
-            time_spent: timeSpentInMinutes
-        }
-        
-        handlePutSimulado(novoSimulado).then((result) => {
-            if (result !== null) {
-                setSimulado(simulado);
-                setIsSimuladoAwaitActive(false);
-            }
-            return true;
-        })
-
-        const respostasIds2: number[] = respostas.map((value) => value[0]);
-        handleIncrementAnswers(respostasIds2);
-        handleIncrementProfileAnswers(totalCorrectAnswers, totalAnswers);
-        handleIncrementProfileMockTest();
-
-        navegate('/history');
-        showAlert("Simulado finalizado com sucesso!", "success");
+        const simulado = await handlePostSimulado(respostas, "automatico", 50);
+        setSimulado(simulado);
         
     }
 
     useEffect(() => {
+        let questions: questionInterface[] = [];
         const getQuestions = async () => {
-                let questions: questionInterface[] = await generateNewSimulado();
+            
+                questions = await generateNewSimulado(10);
                 
-                return questions;
+                const answers = await handleGetAnswersByQuestionsIds(questions.map(q => q.id));
+
+                questions.forEach(question => {
+                    let qstAnswer: respostaInterface[] | undefined = answers.filter(ans => ans.question_id === question.id);
+                    //Organiza as alternativas na ordem "A" "B" "C" "D" "E", por answer.question_letter;
+                    question.answers = qstAnswer?.sort((a, b) => a.question_letter.charCodeAt(0) - b.question_letter.charCodeAt(0));
+                    setQuestionsHashMap(questions);
+                    
+
+                    setLoading(false);
+
+            }); 
         }
-
-
-
-        const postSimulado = async (questions: questionInterface[]) => {
+        getQuestions();
+        
+        const postSimulado = async () => {
             const simulado = await handlePostSimulado(questions, "automatico", 50);
             if (simulado !== null) {
                 setSimulado(simulado);
-                setLoading(false);
             }
         }
-
-
-        getQuestions().then((questions) => {
-            handleGetAnswersByQuestionsIds(questions.map((question) => question.id)).then((answers) => {
-                const questionsHashMap: questionMapInterface = questions.map((question, ) => {
-                    let newAnswers = answers.filter((answer) => answer.question_id === question.id)
-                    let newOrderedAnswers = newAnswers.sort((a, b) => a.question_letter.localeCompare(b.question_letter));
-                    return {
-                        question: question,
-                        answers: newOrderedAnswers              
-                    }
-                });
-                setQuestionsHashMap(questionsHashMap);
-                postSimulado(questions);
-            });
-        });
-
-
-        
+        postSimulado();
     }, []);
 
-    // function returnJSXOverlay(): JSX.Element{
+    function returnJSXOverlay(): JSX.Element{
 
-    //     if (isSimuladoAwaitActive === null) {
-    //         return (
-    //         <div id="historyOverlay">
-    //             <div id="loading">
-    //                 <h2>Tudo pronto!</h2>
-    //                 <p>Aguarde enquanto corrigimos sua prova...</p>
-    //                 <Spinner size="xl" />
-    //             </div>
-    //         </div>
-    //         )
-    //     }
-    //     else{
-    //         return (
-    //         <div id="historyOverlay">
-    //                 <h2>Simulado #{simulado.id}</h2>
-    //                 <p>Tempo consumudo: {simulado.time_spent} minutos</p>
-    //                 <p>Feito dia: {date.format(simulado.creation_date, 'DD/MM/YYYY')}</p>
-    //                 <h3>{simulado.total_correct_answers}/{simulado.total_answers}</h3>
-    //                 <div className="progress">
-    //                     <div style={{width: (simulado.total_correct_answers * 100 / simulado.total_answers ) + '%'}}></div>
-    //                 </div>
-    //                 <div className="materias">
-    //                     {
-    //                         Object.keys(simulado.subjects).map((subject, index) => {
-    //                             return (
-    //                                 <div key={index}>
-    //                                     <p>{subject} [{simulado.subjects[subject].total_correct_answers}/{simulado.subjects[subject].total_answers}]</p>
-    //                                     <div className="progress">
-    //                                         <div style={{width: (simulado.subjects[subject].total_correct_answers * 100 / simulado.subjects[subject].total_answers ) + '%'}} />
-    //                                     </div>
-    //                                 </div>
-    //                             )
-    //                         })
-    //                     }
-    //                 </div>
-    //         </div>
-    //         )
-    //     }
+        if (isSimuladoFinished === null) {
+            return (
+            <div id="historyOverlay">
+                <div id="loading">
+                    <h2>Tudo pronto!</h2>
+                    <p>Aguarde enquanto corrigimos sua prova...</p>
+                    <Spinner size="xl" />
+                </div>
+            </div>
+            )
+        }
+        else{
+            return (
+            <div id="historyOverlay">
+                    <h2>Simulado #{simulado.id}</h2>
+                    <p>Tempo consumudo: {simulado.time_spent} minutos</p>
+                    <p>Feito dia: {date.format(simulado.creation_date, 'DD/MM/YYYY')}</p>
+                    <h3>{simulado.total_correct_answers}/{simulado.total_answers}</h3>
+                    <div className="progress">
+                        <div style={{width: (simulado.total_correct_answers * 100 / simulado.total_answers ) + '%'}}></div>
+                    </div>
+                    <div className="materias">
+                        {
+                            Object.keys(simulado.subjects).map((subject, index) => {
+                                return (
+                                    <div key={index}>
+                                        <p>{subject} [{simulado.subjects[subject].total_correct_answers}/{simulado.subjects[subject].total_answers}]</p>
+                                        <div className="progress">
+                                            <div style={{width: (simulado.subjects[subject].total_correct_answers * 100 / simulado.subjects[subject].total_answers ) + '%'}} />
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+            </div>
+            )
+        }
 
-    // }
+    }
 
   
-    //Esse código não tem erro
-    
     return (
-        loading ? <h2>Aguarde enquanto preparamos o seu simulado... </h2> :
+        loading ? <h2>Aguarde enquanto finalizamos o seu simulado... </h2> :
         <>
             {questionsHashMap === null ? <h1>Erro ao carregar simulado</h1> :
                 <Simulado questionsHashMap={questionsHashMap} handleFinishSimulado={handleGetRespostas} 
-                isSimuladoFinished={isSimuladoAwaitActive}   mockTestId={simulado.id}  
+                isSimuladoFinished={isSimuladoAwaitActive}     
                 /> 
             }
+            <Modal
+                isCentered
+                onClose={onClose}
+                isOpen={isOpen}
+                motionPreset='slideInBottom'
+                closeOnEsc={false}
+                closeOnOverlayClick={false}
+            >
+                <ModalOverlay />
+                <ModalContent>
+                <ModalHeader>{simulado === null ? 'Corrigindo...' : 'Informações detalhadas'}</ModalHeader>
+                <ModalBody>
+                    {returnJSXOverlay()}
+                </ModalBody> 
+                    <ModalFooter>
+                        {simulado !== null && <>
+                            <Button variant='ghost' mr={3} onClick={() => {
+                                navegate('/');
+                            }}>Voltar ao home</Button>
+                            <Button colorScheme='blue' onClick={onClose}>
+                            Ver prova
+                            </Button>
+                            </>
+                        }
+
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
         
   )
