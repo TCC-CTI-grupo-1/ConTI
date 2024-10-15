4cf972c7554152943a2e94484e5f401a7e24a23a
Fernando Ellerbrock Theodoro <135258926+Sekqies@users.noreply.github.com>
2024-10-15 07:50:56 -0300

    Adicionado switch para usar remoto, ou não

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,78 +1,77 @@
-// import express from 'express';
-// import cors from 'cors';
-// import { routes } from './http/routes';
-// import dotenv from 'dotenv';
-// import RedisStore from "connect-redis";
-// import session from 'express-session';
-// import cookieParser from 'cookie-parser';
-// import { createClient } from 'redis';
-
-// dotenv.config();
-
-// const app = express();
-// const passRedis = process.env.REDIS_PASS;
-// const hostRedis = process.env.REDIS_HOST;
-// const portRedis = parseInt(process.env.REDIS_PORT as string);
-// let redisClient = createClient({
-//     password: passRedis,
-//     socket: {
-//         host: hostRedis,
-//         port: portRedis
-//     }
-// });
-// redisClient.connect().catch(console.error);
-// let redisStore = new RedisStore({
-//     client: redisClient
-// });
-
-// const urlLocal = 'http://localhost:5173';
-// const urlRemoto = 'https://projetoscti.com.br';
-// const urlCloud = 'http://cti.4edge.cloud:3000';
-
-// app.use(cors({
-//     origin: [urlLocal, urlRemoto, urlCloud],
-//     credentials: true
-// }));
-// app.use(express.json());
-
-// if (process.env.SECRET_KEY === undefined) {
-//     console.error('SECRET_KEY não definida');
-//     process.exit(1);
-// }
-
-// app.use(session({
-//     store: redisStore,
-//     secret: process.env.SECRET_KEY,
-//     resave: false,
-//     saveUninitialized: false,
-//     name: 'sid',
-//     cookie: {
-//         maxAge: 1000 * 60 * 60 * 12,
-//         secure: process.env.NODE_ENV === 'production',
-//         sameSite: 'none'
-//     }
-// }));
-// app.use(cookieParser());
-// app.use(express.static('uploads'));
-
-// routes(app);
-
-// const port = 3000;
-// app.listen(port, () => {
-//     console.log("Server is running on port " + port);
-// });
-
+import RedisStore from "connect-redis";
+import { createClient } from 'redis';
 import express from 'express';
 import cors from 'cors';
 import { routes } from './http/routes';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
+const isRemote = false;
 const session = require('express-session');
 const fs = require('fs');
 const https = require('https');
 const app = express();
+
+if(isRemote) {
+dotenv.config();
+
+const app = express();
+const passRedis = process.env.REDIS_PASS;
+const hostRedis = process.env.REDIS_HOST;
+const portRedis = parseInt(process.env.REDIS_PORT as string);
+let redisClient = createClient({
+    password: passRedis,
+    socket: {
+        host: hostRedis,
+        port: portRedis
+    }
+});
+redisClient.connect().catch(console.error);
+let redisStore = new RedisStore({
+    client: redisClient
+});
+
+const urlLocal = 'http://localhost:5173';
+const urlRemoto = 'https://projetoscti.com.br';
+const urlCloud = 'http://cti.4edge.cloud:3000';
+
+app.use(cors({
+    origin: [urlLocal, urlRemoto, urlCloud],
+    credentials: true
+}));
+app.use(express.json());
+
+if (process.env.SECRET_KEY === undefined) {
+    console.error('SECRET_KEY não definida');
+    process.exit(1);
+}
+
+app.use(session({
+    store: redisStore,
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    name: 'sid',
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 12,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none'
+    }
+}));
+app.use(cookieParser());
+app.use(express.static('uploads'));
+
+routes(app);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
+
+}
+else {
+
+
 
 dotenv.config();
 const urlLocal = 'http://localhost:5173';
@@ -107,3 +106,4 @@ app.use(express.static('uploads'));
 routes(app);
 
 app.listen(port, () => { console.log("Server is running on port 3001")} );
+}
