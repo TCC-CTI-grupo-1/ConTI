5d8ab85f0f4c67b3b1687095d989152d0075d5dd
Mateus Bastazini Nascimento Talhacolo <112733464+MBastazini@users.noreply.github.com>
2024-11-01 10:23:18 -0300

    Merge deploy_server no /server

--- server/src/http/controllers/putArea_ProfileController.ts
+++ server/src/http/controllers/putArea_ProfileController.ts
@@ -3,19 +3,36 @@ import { Area_ProfileDAO } from "../../DAO/Area_ProfileDAO";
 
 import { Request, Response } from "express";
 
+import { createClient } from 'redis';
+
+const passRedis = process.env.REDIS_PASS;
+const hostRedis = process.env.REDIS_HOST;
+const portRedis = parseInt(process.env.REDIS_PORT as string);
+let redisClient = createClient({
+    password: passRedis,
+    socket: {
+        host: hostRedis,
+        port: portRedis
+    }
+});
+redisClient.connect();
+
 export async function incrementArea_ProfileController(req: Request, res: Response) {
     const area_profileDAO = new Area_ProfileDAO();
-    if(req.session === undefined) {
-        return res.status(404).json({ message: 'Sessão não inicializada' });
-    }
-    if (!req.session.isLoggedIn) {
-        return res.status(401).json({ message: 'Usuário não logado' });
-    }
-    if(req.session.profile === undefined) {
-        return res.status(404).json({ message: 'Perfil não encontrado' });
-    }
     try {
-        await area_profileDAO.incrementAreas_Profile(req.session.profile.id, req.body.area_ids);
+        const userId = req.params.uuid;
+    
+        const profileString = await redisClient.get(`profile:${userId}`);
+
+        let profile = null;
+        if (profileString) {
+        profile = JSON.parse(profileString); // Parse the string into an object
+        }
+
+        if(profile === null) {
+            return res.status(404).json({ message: 'Sessão não inicializada' });
+        }
+        await area_profileDAO.incrementAreas_Profile(profile.id, req.body.area_ids);
         res.json({ message: 'Áreas incrementadas com sucesso' });
     } catch (error: any) {
         res.status(500).json({ message: error.message });
@@ -24,17 +41,21 @@ export async function incrementArea_ProfileController(req: Request, res: Respons
 
 export async function incrementAreas_ProfileController(req: Request, res: Response) {
     const area_profileDAO = new Area_ProfileDAO();
-    if(req.session === undefined) {
-        return res.status(404).json({ message: 'Sessão não inicializada' });
-    }
-    if (!req.session.isLoggedIn) {
-        return res.status(401).json({ message: 'Usuário não logado' });
-    }
-    if(req.session.profile === undefined) {
-        return res.status(404).json({ message: 'Perfil não encontrado' });
-    }
+    
     try {
-        await area_profileDAO.incrementAreas_Profile(req.session.profile.id, req.body.areasAndAnswers);
+        const userId = req.params.uuid;
+    
+        const profileString = await redisClient.get(`profile:${userId}`);
+
+        let profile = null;
+        if (profileString) {
+        profile = JSON.parse(profileString); // Parse the string into an object
+        }
+
+        if(profile === null) {
+            return res.status(404).json({ message: 'Sessão não inicializada' });
+        }
+        await area_profileDAO.incrementAreas_Profile(profile.id, req.body.areasAndAnswers);
         res.json({ message: 'Áreas incrementadas com sucesso' });
     } catch (error: any) {
         res.status(500).json({ message: error.message });
